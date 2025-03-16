bf39ce0c14f6770e2ae0973262db4cd563b30af5
BlenSeleshi <bluseleshi@gmail.com>
2025-03-16 17:22:18 +0300

    script for trader categorization

---
+++ scripts/trader_categorization.py
@@ -0,0 +1,15 @@
+from sklearn.cluster import KMeans
+import pandas as pd
+import logging
+from scripts.utils import configure_logging
+
+configure_logging()
+
+def categorize_traders(df, num_clusters=5):
+    logging.info("Starting trader categorization...")
+
+    trader_data = df.groupby("Trader")["CIF/FOB Value (ETB)"].sum().reset_index()
+    trader_data["Trader Category"] = KMeans(n_clusters=num_clusters, random_state=42, n_init=10).fit_predict(trader_data[["CIF/FOB Value (ETB)"]])
+
+    logging.info("Trader categorization completed.")
+    return trader_data
